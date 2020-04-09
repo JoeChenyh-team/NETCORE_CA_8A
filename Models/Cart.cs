986af9f78d952f61c4478def8c798da4257b8721
wyr1527891 <wu.yi.rui.91@gmail.com>
2020-04-09 15:14:45 +0800

    Added Cart property requirements

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NETCORE_CA_8A.Models
 {
     public class Cart
     {
+        [MaxLength(36)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Id { get; set; }
 
+        [Required]
+        [MaxLength(3)]
         public int Quantity { get; set; }
 
+        [Required]
+        [MaxLength(36)]
         public System.DateTime DateCreated { get; set; }
 
+        [Required]
+        [MaxLength(36)]
         public int ProductId { get; set; }
 
         public virtual Product Product { get; set; }
