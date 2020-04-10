7f758137b26ffc1c7999414db945c73ceca333b5
Yihan Johaness Chen <e0533420@u.nus.edu>
2020-04-10 12:35:22 +0800

    Cart code

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -9,6 +9,16 @@ namespace NETCORE_CA_8A.Models
 {
     public class Cart
     {
+        //Joe: insert AppDB Service?? Not sure how to do it but I copy this from online. 
+        /*
+        private readonly AppDbContext _appDbContext;
+        private Cart(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        */
+
+
         [MaxLength(36)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Id { get; set; }
@@ -33,6 +43,23 @@ namespace NETCORE_CA_8A.Models
 
         public virtual Product Product { get; set; }
         public virtual Customer Customer { get; set; }
+    
+    /* From online as well:
+     public static Cart GetCart(IServiceProvider services)
+     {
+     ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+     .HttpContext.Session;
+
+    var context = services.GetService<AppDbContext>();
+    string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+    session.SetString("CartId", cartId);
+
+    return new ShoppingCart(context) {CartId = cartId);
+    }
+    */
+     
+    
     }
 
     
--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace NETCORE_CA_8A.Models
-{
+{ 
+    
     public class ProductModel
     {
         private List<Product> products;
